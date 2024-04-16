285343994bd0cafc371ba25c7d00effae31ab1b2
5teef <mustafaquadan@gmail.com>
2024-04-17 00:55:44 +0200

    Homepage CarEight
    Added bootstrap flex

--- Client/src/components/HomeCarsSale8.jsx
+++ Client/src/components/HomeCarsSale8.jsx
@@ -19,11 +19,15 @@ export default function CarsEight() {
     loadCars();
   }, []);
 
+  const formatPrice = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
+
   return (
     <Container>
       <Row className="careight-container">
         {cars.map(car => (
-          <Col key={car.slug} xs={12} sm={6} md={4} lg={4} xl={3} className="car-section"> {/* Tre kolumner på lg, xl skärmar */}
+          <Col key={car.slug} xs={12} sm={6} md={4} lg={4} className="car-section">
             <Link to={{ pathname: `/car/${car.slug}` }} style={{ textDecoration: 'none' }}>
               <img src={car.img} alt={car.title} className="car-image img-fluid" />
               <div className="car">
@@ -31,16 +35,16 @@ export default function CarsEight() {
                 <div><p className="cartext">{car.undertitle}</p></div>
               </div>
               <div className="car-info">
-                <div><p className="carprice">{car.price} SEK</p></div>
+                <div><p className="carprice">{formatPrice(car.price)} SEK</p></div>
                 <div><p className="caryear">{car.year}</p></div>
                 <div><p className="carmile">{car.mil} mil</p></div>
                 <div><p className="cargear">{car.vaxel}</p></div>
                 <div><p className="carbransle">{car.bransle}</p></div>
               </div>
+            </Link>
               <div className="line-separator-car">
                 <img src={Line} alt={car.title} />
               </div>
-            </Link>
           </Col>
         ))}
       </Row>

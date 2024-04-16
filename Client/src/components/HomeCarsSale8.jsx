a4a3e3647ef75d00f30e8b6def185e5f0c3c6ba7
5teef <mustafaquadan@gmail.com>
2024-04-16 18:27:52 +0200

    Added display/grid to the eightcars.

--- Client/src/components/HomeCarsSale8.jsx
+++ Client/src/components/HomeCarsSale8.jsx
@@ -1,48 +1,49 @@
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import Line from "/img/line.png"
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap';
+import Line from "/img/line.png";
 
 export default function CarsEight() {
-  const [cars, setItems] = useState([])
+  const [cars, setItems] = useState([]);
 
   useEffect(() => {
-    async function load() {
-      const response = await fetch("api/CarsEight")
-      const data = await response.json()
-      console.log(data)
-      setItems(data)
+    async function loadCars() {
+      try {
+        const response = await fetch("api/CarsEight");
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error("Error fetching cars:", error);
+      }
     }
-    load()
-  }, [])
+    loadCars();
+  }, []);
 
   return (
-      <div className="careight-container">
+    <Container>
+      <Row className="careight-container">
         {cars.map(car => (
-          <section key={car.slug} className="car-section">
-              <Link to={{ pathname: `/car/${car.slug}` }} style={{ textDecoration: 'none' }}>
-                <img src={car.img} alt={car.title} className="car-image" />
-                <div className="car">
+          <Col key={car.slug} xs={12} sm={6} md={4} lg={4} xl={3} className="car-section"> {/* Tre kolumner på lg, xl skärmar */}
+            <Link to={{ pathname: `/car/${car.slug}` }} style={{ textDecoration: 'none' }}>
+              <img src={car.img} alt={car.title} className="car-image img-fluid" />
+              <div className="car">
                 <h2>{car.title}</h2>
                 <div><p className="cartext">{car.undertitle}</p></div>
-                </div>
-                <div className="car-info">
+              </div>
+              <div className="car-info">
                 <div><p className="carprice">{car.price} SEK</p></div>
                 <div><p className="caryear">{car.year}</p></div>
                 <div><p className="carmile">{car.mil} mil</p></div>
                 <div><p className="cargear">{car.vaxel}</p></div>
                 <div><p className="carbransle">{car.bransle}</p></div>
-                </div>
-                <div className="line-seperator-car">
-                  <img src={Line} alt={car.title} />
-                </div>
-              </Link>
-
-
-            
-          </section>
+              </div>
+              <div className="line-separator-car">
+                <img src={Line} alt={car.title} />
+              </div>
+            </Link>
+          </Col>
         ))}
-      </div>
-
-  )
+      </Row>
+    </Container>
+  );
 }

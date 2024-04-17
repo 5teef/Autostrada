0092a7123593fd0d4169d649ace88b765e62f1b4
5teef <mustafaquadan@gmail.com>
2024-04-18 00:50:57 +0200

    Homepage
    Added bootstrap flexbox with some css to Carscontainer

--- Client/src/components/HomeCarsSale8.jsx
+++ Client/src/components/HomeCarsSale8.jsx
@@ -27,24 +27,34 @@ export default function CarsEight() {
     <Container>
       <Row className="careight-container">
         {cars.map(car => (
-          <Col key={car.slug} xs={12} sm={6} md={4} lg={4} className="car-section">
+          <Col key={car.slug} xs={12} sm={6} md={4} className="car-section">
             <Link to={{ pathname: `/car/${car.slug}` }} style={{ textDecoration: 'none' }}>
               <img src={car.img} alt={car.title} className="car-image img-fluid" />
               <div className="car">
                 <h2>{car.title}</h2>
                 <div><p className="cartext">{car.undertitle}</p></div>
               </div>
-              <div className="car-info">
-                <div><p className="carprice">{formatPrice(car.price)} SEK</p></div>
-                <div><p className="caryear">{car.year}</p></div>
-                <div><p className="carmile">{car.mil} mil</p></div>
-                <div><p className="cargear">{car.vaxel}</p></div>
-                <div><p className="carbransle">{car.bransle}</p></div>
-              </div>
             </Link>
-              <div className="line-separator-car">
-                <img src={Line} alt={car.title} />
-              </div>
+            <Row className="car-info justify-content-space-between" >
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="carprice text-nowrap">{formatPrice(car.price)} SEK</p></div>
+              </Col>
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="caryear text-nowrap">{car.year}</p></div>
+              </Col>
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="carmile text-nowrap">{car.mil} mil</p></div>
+              </Col>
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="cargear text-nowrap">{car.vaxel}</p></div>
+              </Col>
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="carbransle text-nowrap">{car.bransle}</p></div>
+              </Col>
+            </Row>
+            <div className="line-separator-car">
+              <img src={Line} alt={car.title} />
+            </div>
           </Col>
         ))}
       </Row>

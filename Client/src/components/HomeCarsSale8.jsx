d50cf94835904e4581db2d2cda6acf09fa9c4896
5teef <mustafaquadan@gmail.com>
2024-04-18 15:30:05 +0200

    restoring to before githubpages!

--- Client/src/components/HomeCarsSale8.jsx
+++ Client/src/components/HomeCarsSale8.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from 'react-bootstrap';
 import Line from "/img/line.png";
@@ -24,6 +24,40 @@ export default function CarsEight() {
   };
 
   return (
-  <h1>Test</h1>
+    <Container>
+      <Row className="careight-container">
+        {cars.map(car => (
+          <Col key={car.slug} xs={12} sm={6} md={4} className="car-section">
+            <Link to={{ pathname: `/car/${car.slug}` }} style={{ textDecoration: 'none' }}>
+              <img src={car.img} alt={car.title} className="car-image img-fluid" />
+              <div className="car">
+                <h2>{car.title}</h2>
+                <div><p className="cartext">{car.undertitle}</p></div>
+              </div>
+            </Link>
+            <Row className="car-info justify-content-space-between" >
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="carprice text-nowrap">{formatPrice(car.price)} SEK</p></div>
+              </Col>
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="caryear text-nowrap">{car.year}</p></div>
+              </Col>
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="carmile text-nowrap">{car.mil} mil</p></div>
+              </Col>
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="cargear text-nowrap">{car.vaxel}</p></div>
+              </Col>
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div><p className="carbransle text-nowrap">{car.bransle}</p></div>
+              </Col>
+            </Row>
+            <div className="line-separator-car">
+              <img src={Line} alt={car.title} />
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }

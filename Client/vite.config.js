85f0c7a41323f7fa261854de530598ccb7e0ebc4
5teef <mustafaquadan@gmail.com>
2024-04-18 15:04:08 +0200

    Github pages.
    Deploying page to githubPage

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Autostrada-Build/",
   server: {
     proxy: {
       // localhost:5173/api -> localhost:3000/,

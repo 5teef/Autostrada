d28d4d96123a4e1762e79aa7a35096516c0e9831
5teef <mustafaquadan@gmail.com>
2024-04-28 21:20:07 +0200

    GoogleMaps
    Changed from marker to advancedmarker

--- Client/src/components/GoogleMaps.jsx
+++ Client/src/components/GoogleMaps.jsx
@@ -1,22 +1,31 @@
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, AdvancedMarker, InfoWindow } from '@vis.gl/react-google-maps';
 
 const API_KEY =
   globalThis.GOOGLE_MAPS_API_KEY ?? (import.meta.env.VITE_GOOGLE_MAPS_API_KEY)
 
+const MAP_KEY =
+  globalThis.GOOGLE_MAPS_API_KEY ?? (import.meta.env.VITE_PUBLIC_MAP_ID)
+
 const GoogleMaps = () => (
   <APIProvider apiKey={API_KEY}>
     <div style={{ height: "50vh", width: "80%", marginTop: "30px" }}>
-    <Map
-      defaultZoom={15}
+      <Map
+        defaultZoom={15}
         defaultCenter={{ lat: 55.832039, lng: 13.327660 }}
-      gestureHandling={'greedy'}
-      disableDefaultUI={true}
-    />
-      <Marker
-        position={{ lat: 55.832039, lng: 13.327660 }}
-      />
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+        mapId={MAP_KEY}
+      >
+
+        <AdvancedMarker
+          position={{ lat: 55.832039, lng: 13.327660 }} />
+
+        <InfoWindow >
+          <h2> test</h2>
+        </InfoWindow>
+
+      </Map>
     </div>
   </APIProvider>
 );
 export default GoogleMaps;
-

c3e2e4756903170389532f9f3dc5f6639e1ca192
5teef <mustafaquadan@gmail.com>
2024-04-30 01:07:42 +0200

    Restore
    Restoring to last commit

--- Client/src/components/HomeCarsSale8.jsx
+++ Client/src/components/HomeCarsSale8.jsx
@@ -1,3 +1,86 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap';
+import Left from "/img/left.png";
+import Button from 'react-bootstrap/Button';
+
+export default function CarsEight() {
+  const [cars, setItems] = useState([]);
+
+  useEffect(() => {
+    async function loadCars() {
+      try {
+        const response = await fetch("/api/cars");
+        const data = await response.json();
+        setItems(data.slice(0, 8));
+      } catch (error) {
+        console.error("Error fetching cars:", error);
+      }
+    }
+    loadCars();
+  }, []);
+
+  const formatPrice = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scrolla till toppen när cars uppdateras
+  }, [cars]);
+
+  return (
+    <>
+      <Container>
+        <Row className="careight-container">
+          {cars.map(car => (
+            <Col key={car.slug} xs={12} sm={6} md={6} lg={4} xl={3} className="car-section">
+              <Link to={{ pathname: `/Autostrada/fordon/${car.slug}` }} style={{ textDecoration: 'none' }}>
+                <img src={car.img} alt={car.title} className="car-image img-fluid" />
+                <div className="car">
+                  <h2>{car.title}</h2>
+                  <div><p className="cartext">{car.undertitle}</p></div>
+                </div>
+              </Link>
+              <Row className="car-info justify-content-space-between" >
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="carprice text-nowrap">{formatPrice(car.price)} SEK</p></div>
+                </Col>
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="caryear text-nowrap">{car.year}</p></div>
+                </Col>
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="carmile text-nowrap">{car.mil} mil</p></div>
+                </Col>
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="cargear text-nowrap">{car.vaxel}</p></div>
+                </Col>
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="carbransle text-nowrap">{car.bransle}</p></div>
+                </Col>
+              </Row>
+              <div className="line-separator-car">
+                <img src={Left} alt="line" />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+
+      <div className="morecars-div">
+        <Link to="/Autostrada/fordon">
+          <Button className="morecars" variant="secondary" size="lg" style={{ textDecoration: 'none' }}>
+            Fler bilar!
+          </Button>
+        </Link>
+      </div>
+
+    </>
+  );
+}
+
+
+
+/*
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from 'react-bootstrap';
 import Left from "/img/left.png";
@@ -12,9 +95,8 @@ export default function CarsEight() {
     <>
       <Container>
         <Row className="careight-container">
-          {/* Bil 1 */}
           <Col xs={12} sm={6} md={6} lg={4} xl={3} className="car-section">
-            <Link to="/car/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
+            <Link to="/fordon/BMW-ff-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
               <img src="/Autostrada/cars/BMW-550.jpg" alt="BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD" className="car-image img-fluid" />
               <div className="car">
                 <h2>BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD</h2>
@@ -43,9 +125,8 @@ export default function CarsEight() {
             </div>
           </Col>
 
-          {/* Bil 2 */}
           <Col xs={12} sm={6} md={6} lg={4} xl={3} className="car-section">
-            <Link to="/car/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
+            <Link to="/fordon/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
               <img src="/Autostrada/cars/BMW-550.jpg" alt="BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD" className="car-image img-fluid" />
               <div className="car">
                 <h2>BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD</h2>
@@ -74,9 +155,8 @@ export default function CarsEight() {
             </div>
           </Col>
 
-          {/* Bil 3 */}
           <Col xs={12} sm={6} md={6} lg={4} xl={3} className="car-section">
-            <Link to="/car/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
+            <Link to="/fordon/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
               <img src="/Autostrada/cars/BMW-550.jpg" alt="BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD" className="car-image img-fluid" />
               <div className="car">
                 <h2>BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD</h2>
@@ -105,9 +185,8 @@ export default function CarsEight() {
             </div>
           </Col>
 
-          {/* Bil 4 */}
           <Col xs={12} sm={6} md={6} lg={4} xl={3} className="car-section">
-            <Link to="/car/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
+            <Link to="/fordon/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
               <img src="/Autostrada/cars/BMW-550.jpg" alt="BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD" className="car-image img-fluid" />
               <div className="car">
                 <h2>BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD</h2>
@@ -136,9 +215,8 @@ export default function CarsEight() {
             </div>
           </Col>
 
-          {/* Bil 5 */}
           <Col xs={12} sm={6} md={6} lg={4} xl={3} className="car-section">
-            <Link to="/car/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
+            <Link to="/fordon/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
               <img src="/Autostrada/cars/BMW-550.jpg" alt="BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD" className="car-image img-fluid" />
               <div className="car">
                 <h2>BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD</h2>
@@ -167,9 +245,8 @@ export default function CarsEight() {
             </div>
           </Col>
 
-          {/* Bil 6 */}
           <Col xs={12} sm={6} md={6} lg={4} xl={3} className="car-section">
-            <Link to="/car/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
+            <Link to="/fordon/BMW-M550-d-xDrive-Touring-Ultimate-Edition-FULLUTRUSTAD" style={{ textDecoration: 'none' }}>
               <img src="/Autostrada/cars/BMW-550.jpg" alt="BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD" className="car-image img-fluid" />
               <div className="car">
                 <h2>BMW M550-d xDrive Touring Ultimate Edition FULLUTRUSTAD</h2>
@@ -212,10 +289,11 @@ export default function CarsEight() {
   );
 }
 
+*/
 
 /*
 
-    import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from 'react-bootstrap';
 import Left from "/img/left.png";
@@ -224,62 +302,44 @@ export default function CarsEight() {
   const [cars, setItems] = useState([]);
 
   useEffect(() => {
-    async function loadCars() {
-      try {
-        const response = await fetch("/api/CarsEight");
-        const data = await response.json();
-        setItems(data);
-      } catch (error) {
-        console.error("Error fetching cars:", error);
-      }
+    async function load() {
+      const response = await fetch("api/CarsEight")
+      const data = await response.json()
+      console.log(data)
+      setItems(data)
     }
-    loadCars();
-  }, []);
-
-  const formatPrice = (price) => {
-    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-  };
+    load()
+  }, [])
 
   return (
-    <>
-    <Container>
-      <Row className="careight-container">
-        {cars.map(car => (
-          <Col key={car.slug} xs={12} sm={6} md={4} className="car-section">
-            <Link to={{ pathname: `/car/${car.slug}` }} style={{ textDecoration: 'none' }}>
-              <img src={car.img} alt={car.title} className="car-image img-fluid" />
-              <div className="car">
-                <h2>{car.title}</h2>
-                <div><p className="cartext">{car.undertitle}</p></div>
-              </div>
-            </Link>
-            <Row className="car-info justify-content-space-between" >
-              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
-                <div><p className="carprice text-nowrap">{formatPrice(car.price)} SEK</p></div>
-              </Col>
-              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
-                <div><p className="caryear text-nowrap">{car.year}</p></div>
-              </Col>
-              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
-                <div><p className="carmile text-nowrap">{car.mil} mil</p></div>
-              </Col>
-              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
-                <div><p className="cargear text-nowrap">{car.vaxel}</p></div>
-              </Col>
-              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
-                <div><p className="carbransle text-nowrap">{car.bransle}</p></div>
-              </Col>
-            </Row>
-            <div className="line-separator-car">
-              <img src={Left} alt="line" />
-            </div>
-          </Col>
+    <div className="careight-container">
+      {cars.map(car => (
+          <Col key={car.slug} xs={12} sm={6} md={6} lg={4} xl={3} className="car-section">
+                <Link to={{ pathname: `/car/${car.slug}` }} style={{ textDecoration: 'none' }}>
+                  <img src={car.img} alt={car.title} className="car-image img-fluid" />
+                  <div className="car">
+                    <h2>{car.title}</h2>
+                    <div><p className="cartext">{car.undertitle}</p></div>
+                  </div>
+                  <div className="car-info">
+                    <div><p className="carprice">{car.price} SEK</p></div>
+                    <div><p className="caryear">{car.year}</p></div>
+                    <div><p className="carmile">{car.mil} mil</p></div>
+                    <div><p className="cargear">{car.vaxel}</p></div>
+                    <div><p className="carbransle">{car.bransle}</p></div>
+                  </div>
+                  <div className="line-seperator-car">
+                    <img src={Line} alt={car.title} />
+                  </div>
+                </Link>
+
+
+
+              </section>
         ))}
-      </Row>
-    </Container>
-    
-    </>
-  );
+            </div>
+
+            )
 }
 
 */
--- Client/src/pages/Fordon.jsx
+++ Client/src/pages/Fordon.jsx
@@ -1,12 +1,80 @@
-//import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap';
+import Left from "/img/left.png";
+import Button from 'react-bootstrap/Button';
 
-export default function Fordon() {
+export default function CarsEight() {
+  const [cars, setItems] = useState([]);
+
+  useEffect(() => {
+    async function loadCars() {
+      try {
+        const response = await fetch("/api/cars");
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error("Error fetching cars:", error);
+      }
+    }
+    loadCars();
+  }, []);
+
+  const formatPrice = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scrolla till toppen när cars uppdateras
+  }, [cars]);
 
   return (
     <>
-    <h1>Fordon i lager</h1>
+      <Container>
+        <Row className="careight-container">
+          {cars.map(car => (
+            <Col key={car.slug} xs={12} sm={6} md={6} lg={4} xl={3} className="car-section">
+              <Link to={{ pathname: `/Autostrada/fordon/${car.slug}` }} style={{ textDecoration: 'none' }}>
+                <img src={car.img} alt={car.title} className="car-image img-fluid" />
+                <div className="car">
+                  <h2>{car.title}</h2>
+                  <div><p className="cartext">{car.undertitle}</p></div>
+                </div>
+              </Link>
+              <Row className="car-info justify-content-space-between" >
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="carprice text-nowrap">{formatPrice(car.price)} SEK</p></div>
+                </Col>
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="caryear text-nowrap">{car.year}</p></div>
+                </Col>
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="carmile text-nowrap">{car.mil} mil</p></div>
+                </Col>
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="cargear text-nowrap">{car.vaxel}</p></div>
+                </Col>
+                <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                  <div><p className="carbransle text-nowrap">{car.bransle}</p></div>
+                </Col>
+              </Row>
+              <div className="line-separator-car">
+                <img src={Left} alt="line" />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+
+      <div className="morecars-div">
+        <Link to="/Autostrada/fordon">
+          <Button className="morecars" variant="secondary" size="lg" style={{ textDecoration: 'none' }}>
+            Fler bilar!
+          </Button>
+        </Link>
+      </div>
+
     </>
-
-  )
-
+  );
 }
+

c45c707b06fa579d0fad90e927aef49434f8bbce
Steef <mustafaquadan@gmail.com>
2024-12-11 20:24:08 +0100

    Designed fordondetails

--- Client/src/components/FordonDetail/FordonDetail.jsx
+++ Client/src/components/FordonDetail/FordonDetail.jsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Slider from "react-slick";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTag, faCalendarAlt, faGasPump, faCogs, faPalette, faTachometerAlt, faBolt, faCarSide, faIdCard } from '@fortawesome/free-solid-svg-icons';
-
-
-import SeperatorLeft from "../Seperator/SeperatorSite_Left";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTag,
+  faCalendarAlt,
+  faGasPump,
+  faPalette,
+  faTachometerAlt,
+  faBolt,
+  faCarSide,
+  faIdCard,
+} from "@fortawesome/free-solid-svg-icons";
 import "./fordondetail.css";
 
 export default function FordonDetail() {
@@ -27,7 +33,7 @@ export default function FordonDetail() {
           throw new Error("Car not found");
         }
         const data = await response.json();
-        setCar(data);
+        setCar(parseCarData(data));
       } catch (err) {
         console.error("Error fetching car details:", err);
         setError(err.message);
@@ -45,126 +51,141 @@ export default function FordonDetail() {
     return <p>Loading...</p>;
   }
 
+  // Function to parse and structure the car data
+  function parseCarData(car) {
+    const description =
+      car.utr?.match(/Nu har vi fått.*?(?=Utrustad med bland annat:)/s)?.[0]?.trim() ||
+      "Ingen beskrivning tillgänglig";
+
+    const equipmentMatch = car.utr?.match(
+      /Utrustad med bland annat:(.*?)(?=AUTOSTRADA|$)/s
+    )?.[1];
+
+    const compactEquipmentMatch = car.utr?.match(/(12V-UTTAG.*)$/s)?.[0];
+
+    const equipment = equipmentMatch
+      ? equipmentMatch
+        .split(/-\s*/)
+        .map((item) => item.trim())
+        .filter(Boolean)
+      : [];
+
+    const compactEquipment = compactEquipmentMatch
+      ? compactEquipmentMatch
+        .split(/,\s*/)
+        .map((item) => item.trim())
+        .filter(
+          (item) =>
+            !equipment.includes(item) && // Undvik dubbletter
+            !/AUTOSTRADA|KOLLA ERAN SKRÄPPOST/.test(item) && // Ta bort reklam och irrelevanta texter
+            Boolean(item)
+        )
+      : [];
+
+    return {
+      ...car,
+      description,
+      equipment,
+      compactEquipment,
+    };
+  }
+
+
+
+
+  const {
+    bilder = ["/placeholder.jpg"],
+    description,
+    equipment,
+    compactEquipment,
+    marke,
+    modell,
+    modellbeteckning,
+    begartpris,
+    amod,
+    mil,
+    effekthp,
+    farg,
+    drivmedel,
+    kaross,
+    regnr,
+  } = car;
+
   const sliderSettings = {
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    adaptiveHeight: false,
     arrows: true,
-    beforeChange: (oldIndex, newIndex) => setCurrentImageIndex(newIndex),
+    beforeChange: (_, newIndex) => setCurrentImageIndex(newIndex),
   };
-
-  const images = car.bilder?.length > 0 ? car.bilder : ["/placeholder.jpg"];
-
-  const parseUtr = (utrText) => {
-    if (!utrText) return { description: null, equipment: null, compactEquipment: [] };
-
-    // Extract description
-    const descriptionMatch = utrText.match(/Nu har vi fått.*?(?=Utrustad med bland annat:)/s);
-    const description = descriptionMatch ? descriptionMatch[0].trim() : null;
-
-    // Extract equipment
-    const equipmentMatch = utrText.match(/Utrustad med bland annat:(.*?)(?=AUTOSTRADA GRÄVMASKINSVÄGEN 5 I ESLÖV!)/s);
-    const equipment = equipmentMatch
-      ? equipmentMatch[1].split(/-\s*/).map((item) => item.trim()).filter(Boolean)
-      : [];
-
-    // Extract compact equipment
-    const compactEquipmentMatch = utrText.match(/(12V-UTTAG.*)/s);
-    const compactEquipment = compactEquipmentMatch
-      ? compactEquipmentMatch[0]
-        .split(/,\s*/)
-        .map((item) => item.trim())
-        .filter(Boolean)
-      : [];
-
-    return { description, equipment, compactEquipment };
-  };
-
-  const parsedUtr = parseUtr(car.utr);
 
   return (
     <div className="car-detail-grid">
-      <h1>{`${car.marke} ${car.modell}  ${car.modellbeteckning}`}</h1>
-      <SeperatorLeft />
-      <div className="car-specification-section">
-        <div className="slider-and-specs-container">
+      <h1>{`${marke} ${modell} ${modellbeteckning}`}</h1>
 
-          <div className="slider-container">
-
-
-            <Slider {...sliderSettings}>
-              {images.map((image, index) => (
-                <div key={index}>
-                  <img
-                    src={image}
-                    alt={`${car.marke} bild ${index + 1}`}
-                    className="car-image"
-                  />
-                  <p className="image-counter-overlay">
-                    {currentImageIndex + 1}/{images.length}
-                  </p>
+      <div className="slider-and-specs-container">
+        <div className="slider-container">
+          <Slider {...sliderSettings}>
+            {bilder.map((image, index) => (
+              <div key={index} style={{ position: "relative" }}>
+                <img
+                  src={image}
+                  alt={`${marke} bild ${index + 1}`}
+                  className="car-image"
+                />
+                <div className="image-counter-overlay">
+                  {currentImageIndex + 1}/{bilder.length}
                 </div>
-              ))}
-            </Slider>
-          </div>
+              </div>
+            ))}
+          </Slider>
         </div>
-
 
         <div className="specifications-container">
           <h2>Biluppgifter</h2>
           <div className="specifications-columns">
-            <p>
-              <FontAwesomeIcon icon={faTag} /> <strong>Pris:</strong> {car.begartpris.toLocaleString()} SEK
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faCalendarAlt} /> <strong>Årsmodell:</strong> {car.amod}
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faTachometerAlt} /> <strong>Miltal:</strong> {car.mil} mil
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faPalette} /> <strong>Färg:</strong> {car.farg}
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faGasPump} /> <strong>Drivmedel:</strong> {car.drivmedel}
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faCogs} /> <strong>Växellåda:</strong> {car.vaxel}
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faBolt} /> <strong>Effekt:</strong> {car.effekthp} hk
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faCarSide} /> <strong>Kaross:</strong> {car.kaross}
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faCalendarAlt} /> <strong>Registreringsår:</strong> {car.regyear}
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faIdCard} /> <strong>Registreringsnummer:</strong> {car.regnr}
-            </p>
+            <div className="specification-column">
+              <p>
+                <FontAwesomeIcon icon={faTag} /> <strong>Pris:</strong> {begartpris.toLocaleString()} SEK
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faCalendarAlt} /> <strong>Årsmodell:</strong> {amod}
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faTachometerAlt} /> <strong>Miltal:</strong> {mil} mil
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faBolt} /> <strong>Effekt:</strong> {effekthp} hk
+              </p>
+            </div>
+            <div className="specification-column">
+              <p>
+                <FontAwesomeIcon icon={faPalette} /> <strong>Färg:</strong> {farg}
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faGasPump} /> <strong>Drivmedel:</strong> {drivmedel}
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faCarSide} /> <strong>Kaross:</strong> {kaross}
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faIdCard} /> <strong>Regnr:</strong> {regnr}
+              </p>
+            </div>
           </div>
         </div>
-
       </div>
 
       <div className="car-info-columns">
-        <h2>
-          Beskrivning
-          <SeperatorLeft />
-        </h2>
-        <p>{parsedUtr.description || "Ingen beskrivning tillgänglig"}</p>
+        <h2>Beskrivning</h2>
+        <p>{description}</p>
 
-        <h2>
-          Utrustning
-          <SeperatorLeft />
-        </h2>
-        {parsedUtr.equipment.length > 0 ? (
+        <h2>Utrustning</h2>
+        {equipment.length > 0 ? (
           <ul>
-            {parsedUtr.equipment.map((item, index) => (
+            {equipment.map((item, index) => (
               <li key={index}>{item}</li>
             ))}
           </ul>
@@ -172,21 +193,20 @@ export default function FordonDetail() {
           <p>Ingen utrustningsinformation tillgänglig</p>
         )}
 
-        <h2>
-          All Utrustning
-          <SeperatorLeft />
-        </h2>
-        {parsedUtr.compactEquipment.length > 0 ? (
+        <h2>All Utrustning</h2>
+        {compactEquipment.length > 0 ? (
           <div className="equipment-columns">
-            {parsedUtr.compactEquipment.map((item, index) => (
-              <span key={index}>{item}</span>
+            {compactEquipment.map((item, index) => (
+              <div key={index} className="equipment-item">
+                {item}
+              </div>
             ))}
           </div>
         ) : (
           <p>Ingen komprimerad utrustning tillgänglig</p>
         )}
+
       </div>
     </div>
   );
-
 }
--- Client/src/components/Nav/Nav.jsx
+++ Client/src/components/Nav/Nav.jsx
@@ -1,26 +1,23 @@
 import { Link } from "react-router-dom";
-import { Container, Row, Col } from 'react-bootstrap';
-import "./nav.css"
+import "./nav.css";
 
 export default function Nav() {
   return (
-    <nav>
-      <Container className="nav">
-        <Row className="nav-row justify-content-center">
-          <Col xs={12} sm={12} md="auto" lg="auto">
-            <Link to="/" style={{ textDecoration: 'none' }}>HEM</Link>
-          </Col>
-          <Col xs={12} sm={12} md={12} lg="auto">
-            <Link to="/fordon" style={{ textDecoration: 'none' }}>FORDON I LAGER</Link>
-          </Col>
-          <Col xs={12} sm={12} md={12} lg="auto">
-            <Link to="/salj" style={{ textDecoration: 'none' }}>SÄLJ DITT FORDON</Link>
-          </Col>
-          <Col xs={12} sm={12} md={12} lg="auto">
-            <Link to="/kontakt" style={{ textDecoration: 'none' }}>KONTAKT</Link>
-          </Col>
-        </Row>
-      </Container>
+    <nav className="custom-nav">
+      <ul className="nav-list">
+        <li>
+          <Link to="/">HEM</Link>
+        </li>
+        <li>
+          <Link to="/fordon">FORDON I LAGER</Link>
+        </li>
+        <li>
+          <Link to="/salj">SÄLJ DITT FORDON</Link>
+        </li>
+        <li>
+          <Link to="/kontakt">KONTAKT</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
